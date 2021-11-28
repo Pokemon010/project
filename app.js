b64aa8e6eba0bcd87b510dc987833d35e69bcc30
Sergey <pokemon170191@gmail.com>
2021-11-29 01:48:38 +0300

    1 version

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 "use strics"
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
 
-const inChecked = true,
-      inClose = false;
-      console.log(inChecked && inClose)
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false,
+    
+    };
+    const a = +prompt("Один из последних просмотенных фильмов ?","" ),
+          b = prompt("На сколько оцените его ?",""),
+          c = +prompt("Один из последних просмотенных фильмов ?","" ),
+          d = prompt("На сколько оцените его ?","");
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+    console.log(personalMovieDB);
